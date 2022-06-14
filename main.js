ba05c59a559aa130a9382f832ac9860e91736156
sbbbotgh <76098746+sbbbotgh@users.noreply.github.com>
2022-06-14 10:45:08 +0530

    Update main.js

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ function start(){
     objectDetecter = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status : Detecting Objects";
     values = document.getElementById("search").value;
-    if(speechSynthesis.speaking){
+    if(synth.speaking){
         synth.cancel();
     }
 }
